a8526dff65440128b95de6efcfdf5dabce8e0ce6
amaliariska <riskaamalia.mail@gmail.com>
2022-05-20 11:14:20 +0700

    completeness module

--- data-quality-framework/core.py
+++ data-quality-framework/core.py
@@ -3,14 +3,13 @@ from connection.google import GoogleSheet
 from quality.completeness import Completeness
 
 def check_completeness (dataframe) :
-    df_completeness = pandas.DataFrame([])
     # connection_sheet = GoogleSheet('https://docs.google.com/spreadsheets/d/1cx8z0I2xJNPaMSmxU_gzujwjH0fkY8COty_d83_ywxE/',
     #                                    'MASTER DATA')
     # data = connection_sheet.transform_to_dataframe()
-    lst = ['Geeks', 'For', 'Geeks', 'is','portal', 'for', None]
+    lst = ['Geeks', 'For', 'Geeks', 'is','portal', 'for', None, '     \n']
     data = pandas.DataFrame(lst)
-    print(data)
     empty_value = Completeness(data, 'sampling table').check_empty_value()
-    print(empty_value)
 
-    return df_completeness
+    return empty_value
+
+# print(check_completeness(None))
--- data-quality-framework/quality/completeness.py
+++ data-quality-framework/quality/completeness.py
@@ -2,30 +2,42 @@
 Masih yang hanya untuk dataframe
 
 """
-import json
 import pandas as pd
 
 
 
 class Completeness:
     def __init__(self, data: pd.DataFrame, table_name: str):
-        self.result = []
-        self.quality_column = {
+        self.result = {
             'table_name': str,
             'column_name': str,
             'completeness_type': str,
             'total_rows': int,
-            'total_quality_rows': int,
+            'total_cells': int,
+            'total_quality_cells': int,
             'data_percentage': float
         }
         self.data = data
-        self.quality_column['table_name'] = table_name
-        self.quality_column['column_name'] = data.columns.values.tolist()
-        self.quality_column['total_rows'] = len(self.data.index)
+        self.result['table_name'] = table_name
+        self.result['column_name'] = data.columns.values.tolist()
+        self.result['total_rows'] = len(self.data.index)
+        self.result['total_cells'] = len(self.data.index) * len(self.result['column_name'])
+
+    def custom_rules (self) :
+        # yg '' atau space doang dan yang lainnya
+        self.data = self.data.apply(lambda x: x.str.strip())
+
+        try :
+            return self.data.value_counts()['']
+        except :
+            return 0
 
     def check_empty_value(self):
-        self.quality_column['completeness_type'] = 'non_empty_value'
-        # check all cells yang valuenya empty or '' or just only space
-        null_data = self.data.isnull().value_counts()
-        print(null_data)
-        return null_data[True]
+        self.result['completeness_type'] = 'non_empty_value'
+        # self.result['result'] = self.data.isnull().value_counts()
+        # baru yg na
+        self.result['total_quality_cells'] = self.data.count()[0]
+        self.result['total_quality_cells'] = self.result['total_quality_cells'] - self.custom_rules()
+        self.result['data_percentage'] = (self.result['total_quality_cells'] / self.result['total_cells']) * 100
+
+        return self.result
