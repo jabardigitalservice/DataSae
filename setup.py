6f739efa7dbba9aed8729f7b35b6dfbe9dffa087
iyagis <agistyaanugrah@gmail.com>
2022-09-13 10:39:41 +0700

    update version

--- setup.py
+++ setup.py
@@ -680,7 +680,7 @@ setup(
         'datasae.export',
         'datasae.quality'
     ],
-    version='0.1.20',
+    version='0.1.21',
     description='Data quality framework for Ekosistem Data Jabar',
     author="JDS's Data Engineer",
     author_email='jds.dataengineer@gmail.com',
