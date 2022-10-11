c3d5b4c50d13138f3d2d7d949bed30d5f147466c
Agistya Anugrah Dwiutama <agistyaanugrah@gmail.com>
2022-10-11 07:44:01 +0700

    publish version 0.1.27

--- setup.py
+++ setup.py
@@ -680,7 +680,7 @@ setup(
         'datasae.export',
         'datasae.quality'
     ],
-    version='0.1.26',
+    version='0.1.27',
     description='Data quality framework for Ekosistem Data Jabar',
     author="JDS's Data Engineer",
     author_email='jds.dataengineer@gmail.com',
