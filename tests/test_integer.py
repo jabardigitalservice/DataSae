633eacaf24e8b8fc36c26df3afe8ebcd0480417a
Pipin Fitriadi <pipinfitriadi@gmail.com>
2023-09-15 16:44:01 +0700

    Fix: Implementasi negative test untuk feature integer

--- tests/test_integer.py
+++ tests/test_integer.py
@@ -82,6 +82,32 @@ class IntegerTest(unittest.TestCase):
 
         self.assertDictEqual(actual_result, excepted_result, MESSAGE)
 
+    def test_in_range_invalid(self):
+        dummy = pd.concat([
+            pd.DataFrame({'columm': np.random.randint(0, 10, 25)}),
+            pd.DataFrame([
+                {'columm': -5},
+                {'columm': 44}
+            ])
+        ])
+
+        actual_result = Integer(dummy).in_range(-2, 11, 'columm')
+        excepted_result = {
+            'score': .9259259259259259,
+            'valid': 25,
+            'invalid': 2,
+            'warning': {
+                25: create_warning_data(
+                    -5, 'Value should be in the range of -2 and 11'
+                ),
+                26: create_warning_data(
+                    44, 'Value should be in the range of -2 and 11'
+                )
+            }
+        }
+
+        self.assertDictEqual(actual_result, excepted_result, MESSAGE)
+
     def test_is_in_valid(self):
         dummy = pd.DataFrame({'columm': np.random.randint(0, 10, 25)})
 
@@ -91,6 +117,32 @@ class IntegerTest(unittest.TestCase):
             'valid': 25,
             'invalid': 0,
             'warning': {}
+        }
+
+        self.assertDictEqual(actual_result, excepted_result, MESSAGE)
+
+    def test_is_in_invalid(self):
+        dummy = pd.concat([
+            pd.DataFrame({'columm': np.random.randint(0, 10, 25)}),
+            pd.DataFrame([
+                {'columm': -5},
+                {'columm': 44}
+            ])
+        ])
+
+        actual_result = Integer(dummy).is_in(range(10), 'columm')
+        excepted_result = {
+            'score': .9259259259259259,
+            'valid': 25,
+            'invalid': 2,
+            'warning': {
+                25: create_warning_data(
+                    -5, 'Value should be in range(0, 10)'
+                ),
+                26: create_warning_data(
+                    44, 'Value should be in range(0, 10)'
+                )
+            }
         }
 
         self.assertDictEqual(actual_result, excepted_result, MESSAGE)
@@ -108,6 +160,29 @@ class IntegerTest(unittest.TestCase):
 
         self.assertDictEqual(actual_result, excepted_result, MESSAGE)
 
+    def test_not_in_invalid(self):
+        dummy = pd.concat([
+            pd.DataFrame({'columm': np.random.randint(0, 10, 25)}),
+            pd.DataFrame([
+                {'columm': -5},
+                {'columm': 10}
+            ])
+        ])
+
+        actual_result = Integer(dummy).not_in([10], 'columm')
+        excepted_result = {
+            'score': .9629629629629629,
+            'valid': 26,
+            'invalid': 1,
+            'warning': {
+                26: create_warning_data(
+                    10, 'Value should be not in [10]'
+                )
+            }
+        }
+
+        self.assertDictEqual(actual_result, excepted_result, MESSAGE)
+
     def test_length_valid(self):
         dummy = pd.DataFrame({'columm': np.random.randint(0, 10, 25)})
 
@@ -117,6 +192,32 @@ class IntegerTest(unittest.TestCase):
             'valid': 25,
             'invalid': 0,
             'warning': {}
+        }
+
+        self.assertDictEqual(actual_result, excepted_result, MESSAGE)
+
+    def test_length_invalid(self):
+        dummy = pd.concat([
+            pd.DataFrame({'columm': np.random.randint(0, 10, 25)}),
+            pd.DataFrame([
+                {'columm': -5},
+                {'columm': 10}
+            ])
+        ])
+
+        actual_result = Integer(dummy).length(1, 'columm')
+        excepted_result = {
+            'score': .9259259259259259,
+            'valid': 25,
+            'invalid': 2,
+            'warning': {
+                25: create_warning_data(
+                    -5, 'Value should have a length of 1'
+                ),
+                26: create_warning_data(
+                    10, 'Value should have a length of 1'
+                )
+            }
         }
 
         self.assertDictEqual(actual_result, excepted_result, MESSAGE)
