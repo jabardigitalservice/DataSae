fa6f28a8a65e7f449379ceee0eaec4b92d48f840
Adi Bintang Pradana <78474342+adibintangprada@users.noreply.github.com>
2023-09-26 12:10:12 +0700

    Feat: Unit test special character

--- tests/test_string.py
+++ tests/test_string.py
@@ -173,3 +173,56 @@ class StringTest(unittest.TestCase):
         }
 
         self.assertDictEqual(actual_result, expected_result, MESSAGE)
+
+    def test_special_character_valid(self):
+        dummy = pd.DataFrame(
+            {
+                "column": ["Python !", "! Python", "!python",
+                           "!", "!!"]
+                }
+            )
+
+        actual_result = String(dummy).df_column_special_char_contain(
+            "!", "column")
+        expected_result = {
+            "score": 1.0,
+            "valid": 5,
+            "invalid": 0,
+            "warning": {},
+        }
+
+        self.assertDictEqual(actual_result, expected_result, MESSAGE)
+
+    def test_special_character_invalid(self):
+        dummy = pd.DataFrame(
+            {
+                "column": ["!", "? Python", "!python",
+                           3, 3.14]
+                }
+            )
+
+        actual_result = String(dummy).df_column_special_char_contain(
+            "!", "column")
+        expected_result = {
+            "score": 0.4,
+            "valid": 2,
+            "invalid": 3,
+            "warning": {
+                1: create_warning_data(
+                    "? Python",
+                    "Value should be contain to !"
+                ),
+                3: create_warning_data(
+                    3,
+                    WarningDataDetailMessage.STRING_DATA_TYPE,
+                    WarningDataMessage.INVALID_DATA_TYPE,
+                ),
+                4: create_warning_data(
+                    3.14,
+                    WarningDataDetailMessage.STRING_DATA_TYPE,
+                    WarningDataMessage.INVALID_DATA_TYPE,
+                ),
+            },
+        }
+
+        self.assertDictEqual(actual_result, expected_result, MESSAGE)
