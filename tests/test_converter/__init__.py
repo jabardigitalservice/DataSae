2e6f42df6ed91fc4a123f17e3b3053c6e7fd8442
Pipin Fitriadi <pipinfitriadi@gmail.com>
2023-10-14 06:04:32 +0700

    fix: move config from tests.test_converter.test_minio to tests.test_converter.__init__

--- tests/test_converter/__init__.py
+++ tests/test_converter/__init__.py
@@ -3,3 +3,8 @@
 # Copyright (c) Free Software Foundation, Inc. All rights reserved.
 # Licensed under the AGPL-3.0-only License. See LICENSE in the project root
 # for license information.
+
+from datasae.converter import Config
+
+CONFIG_JSON: Config = Config('tests/data/config.json')
+CONFIG_YAML: Config = Config('tests/data/config.yaml')
--- tests/test_converter/test_minio.py
+++ tests/test_converter/test_minio.py
@@ -6,19 +6,20 @@
 
 import unittest
 
-from datasae.converter import Config, DataSourceType, FileType
+from datasae.converter import DataSourceType
+
+from . import CONFIG_JSON, CONFIG_YAML
 
 
 class MinioTest(unittest.TestCase):
     def __init__(self, methodName: str = 'runTest'):
         super().__init__(methodName)
-        self.PATH: str = 'tests/data/config'
         self.NAME: str = 'test_minio'
-        self.minio = Config(f'{self.PATH}{FileType.JSON}')(self.NAME)
+        self.minio = CONFIG_JSON(self.NAME)
 
     def test_config(self):
-        for file_type in (FileType.JSON, FileType.YAML):
-            minio = Config(f'{self.PATH}{file_type}')(self.NAME)
+        for config in (CONFIG_JSON, CONFIG_YAML):
+            minio = config(self.NAME)
             self.assertIs(minio.type, DataSourceType.MINIO)
             self.assertEqual(minio.endpoint, 'play.min.io')
             self.assertEqual(minio.access_key, 'Q3AM3UQ867SPQQA43P2F')
