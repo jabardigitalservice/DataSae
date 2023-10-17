13d51233e1ba5fec26b859fbe9333ac9e0697e6a
Pipin Fitriadi <pipinfitriadi@gmail.com>
2023-10-17 17:27:44 +0700

    fix: datasae.converter.minio.Minio
    CU-86ctrenwy - Reader: MinIO

--- datasae/converter/minio.py
+++ datasae/converter/minio.py
@@ -4,9 +4,14 @@
 # Licensed under the AGPL-3.0-only License. See LICENSE in the project root
 # for license information.
 
+from __future__ import annotations
 from dataclasses import dataclass
+from pandas import DataFrame
+from urllib3 import BaseHTTPResponse
 
-from . import DataSource
+from minio import Minio as MinioClass
+
+from . import DataSource, FileType
 
 
 @dataclass(repr=False)
@@ -14,3 +19,29 @@ class Minio(DataSource):
     endpoint: str
     access_key: str
     secret_key: str
+
+    @property
+    def connection(self) -> MinioClass:
+        return MinioClass(**super().connection)
+
+    def __call__(
+        self, file_type: FileType, *args, **kwargs
+    ) -> DataFrame | bytes:
+        '''
+        Converter from various file type into Pandas DataFrame.
+
+        Args:
+            file_type (FileType): _description_
+
+        Returns:
+            DataFrame | bytes: Pandas DataFrame or bytes if file type not
+                support.
+        '''
+
+        response: BaseHTTPResponse = self.connection.get_object(
+            *args, **kwargs
+        )
+        data = response.data
+        response.close()
+        response.release_conn()
+        return super().__call__(file_type, data)
