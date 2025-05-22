ea20b2329b556a715be0c57a44f8b10b65e2b137
Riska Amalia <amaliariska8@yahoo.co.id>
2025-05-22 08:33:24 +0700

    test: testing git tag

--- datasae/__init__.py
+++ datasae/__init__.py
@@ -661,7 +661,7 @@
 #   You should also get your employer (if you work as a programmer) or school,
 # if any, to sign a "copyright disclaimer" for the program, if necessary.
 # For more information on this, and how to apply and follow the GNU AGPL, see
-# <https://www.gnu.org/licenses/>.
+# <https://www.gnu.org/licenses/>
 
 from datasae.quality.completeness import Completeness as Completeness
 from datasae.quality.uniqueness import Uniqueness as Uniqueness
